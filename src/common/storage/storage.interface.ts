d9060914b2524cd53a5060aa4a584ca4a5841a83
jgodwin10 <easypeasy725@gmail.com>
2025-11-04 18:48:06 +0100

    update

--- src/common/storage/storage.interface.ts
+++ src/common/storage/storage.interface.ts
@@ -1,6 +1,11 @@
-export interface StorageResult { url: string; key?: string; }
+export interface StorageResult {
+	url: string;
+	key?: string;
+	name?: string;
+}
 
 export interface StorageService {
-  uploadFile(file: Express.Multer.File, options?: { folder?: string }): Promise<StorageResult>;
-  delete?(keyOrUrl: string): Promise<void>;
+	uploadFile(file: Express.Multer.File, options?: { folder?: string }): Promise<StorageResult>;
+
+	delete?(keyOrUrl: string): Promise<void>;
 }
